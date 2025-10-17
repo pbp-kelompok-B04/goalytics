a3034adfc1f5af7190378c59392bbb561f163706
juvenile-coder <jefftirza@gmail.com>
2025-10-17 22:26:28 +0700

    CRSF Fix

--- goalytics/settings.py
+++ goalytics/settings.py
@@ -32,7 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jefferson-tirza-goalytics.pbp.cs.ui.ac.id"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://jefferson-tirza-goalytics.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
