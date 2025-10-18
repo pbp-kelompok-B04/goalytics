9f642cd55ff86358989f96a5a8e42956ec8c1079
susshikan <muhamadh123g@gmail.com>
2025-10-18 13:15:45 +0700

    create player model

--- PlayerClub_Data/models.py
+++ PlayerClub_Data/models.py
@@ -9,6 +9,15 @@ class Club(models.Model):
     def __str__(self):
         return self.name
 
+class Player(models.Model):
+    external_id = models.IntegerField(unique=True, help_text="ID pemain dari dataset")
+    name = models.CharField(max_length=200)
+    club = models.ForeignKey(Club, on_delete=models.SET_NULL, null=True, blank=True, related_name='players')
+    position = models.CharField(max_length=50, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 
