54a51a0326963fdd6f2237f266c001bfacb9a383
susshikan <muhamadh123g@gmail.com>
2025-10-18 13:21:18 +0700

    add attribute club

--- PlayerClub_Data/models.py
+++ PlayerClub_Data/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Club(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200, unique=True)
     country = models.CharField(max_length=100, blank=True, null=True)
     code = models.CharField(max_length=50, blank=True, null=True)
@@ -10,7 +11,7 @@ class Club(models.Model):
         return self.name
 
 class Player(models.Model):
-    external_id = models.IntegerField(unique=True)
+    id = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
     club = models.ForeignKey(Club, on_delete=models.SET_NULL, null=True, blank=True, related_name='players')
     position = models.CharField(max_length=50, blank=True, null=True)
