5bba8f49f2e1cab2d8119205d800b1644648ee6d
susshikan <muhamadh123g@gmail.com>
2025-10-18 13:18:23 +0700

    add attribute in player

--- PlayerClub_Data/models.py
+++ PlayerClub_Data/models.py
@@ -4,17 +4,23 @@ class Club(models.Model):
     name = models.CharField(max_length=200, unique=True)
     country = models.CharField(max_length=100, blank=True, null=True)
     code = models.CharField(max_length=50, blank=True, null=True)
-    stadium = models.CharField(max_length=200, blank=True, null=True)  # 🏟️ nama stadion
+    stadium = models.CharField(max_length=200, blank=True, null=True)  
 
     def __str__(self):
         return self.name
 
 class Player(models.Model):
-    external_id = models.IntegerField(unique=True, help_text="ID pemain dari dataset")
+    external_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
     club = models.ForeignKey(Club, on_delete=models.SET_NULL, null=True, blank=True, related_name='players')
     position = models.CharField(max_length=50, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
+    height_cm = models.FloatField(blank=True, null=True)
+    total_goals = models.PositiveIntegerField(default=0)
+    total_assists = models.PositiveIntegerField(default=0)
+    yellow_cards = models.PositiveIntegerField(default=0)
+    red_cards = models.PositiveIntegerField(default=0)
+
 
     def __str__(self):
         return self.name
