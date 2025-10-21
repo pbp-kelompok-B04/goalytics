b3abf4218754b27230d0fa2f3311f3b7330c9be4
susshikan <muhamadh123g@gmail.com>
2025-10-21 18:33:45 +0700

    mapping url

--- forum/urls.py
+++ forum/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('api/posts/', views.get_all_post, name='get_all_post'),
+    path('api/posts/mypost', views.get_my_posts, name='get_my_post'),
+    path('api/posts/<int:post_id>', views.get_post_by_id, name='get_post_by_id'),
+
+    path("api/posts/", views.create_post, name="create_post"),        
+
+    path("api/posts/<int:post_id>/comments/", views.get_post_comment, name="get_post_comment"),     
+    path("api/posts/<int:post_id>/comments/", views.create_comment, name="create_comment"),  
+   
 ]
