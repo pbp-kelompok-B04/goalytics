5ea2044addde9c678f643019cc97d712603a9574
susshikan <muhamadh123g@gmail.com>
2025-10-21 16:36:34 +0700

    create comment model

--- forum/models.py
+++ forum/models.py
@@ -11,3 +11,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Komentar oleh {self.user.username} di {self.post.title}"
