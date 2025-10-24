ce5b12e47dd356188e08057540a83dc993444fda
MALIK KAREEM <malikkareem852@gmail.com>
2025-10-24 13:54:51 +0700

    admin.py impelementation in PlayerClub_Data and users

--- PlayerClub_Data/admin.py
+++ PlayerClub_Data/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Club, Player
+
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("name", "league", "season", "total_goal", "total_assist")
+    search_fields = ("name", "league")
+    list_filter = ("league", "season")
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("name", "position", "club", "goals", "assists")
+    search_fields = ("name", "nation", "club__name")
+    list_filter = ("position", "club__league")
+    autocomplete_fields = ("club",)
