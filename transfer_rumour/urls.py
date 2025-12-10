75334d8580728f5da429e2166c674fefa0bc1642
MALIK KAREEM <malikkareem852@gmail.com>
2025-12-10 21:37:49 +0700

    Settings & URLs adjustments

--- transfer_rumour/urls.py
+++ transfer_rumour/urls.py
@@ -1,6 +1,7 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import path
-
 from . import views
+
 
 app_name = "transfer_rumour"
 
@@ -11,8 +12,9 @@ urlpatterns = [
     path("create/", views.rumour_create, name="create"),
     path("<slug:slug>/edit/", views.rumour_update, name="edit"),
     path("<slug:slug>/delete/", views.rumour_delete, name="delete"),
-    path("<slug:slug>/", views.rumour_detail, name="detail"),
-    path("create-flutter/", views.create_rumour_flutter, name="create_flutter"),
+    path("create-flutter/", (views.create_rumour_flutter), name="create_flutter",),
     path("<slug:slug>/update-flutter/", views.update_rumour_flutter, name="update_flutter"),
     path("<slug:slug>/delete-flutter/", views.delete_rumour_flutter, name="delete_flutter"),
+    path("<slug:slug>/", views.rumour_detail, name="detail"),
+
 ]
