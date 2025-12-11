346896aecb352bd49b73229fbf8dd8de23e02dcd
MALIK KAREEM <malikkareem852@gmail.com>
2025-12-11 10:01:36 +0700

    Perbaikan agar ketika user register melalui flutter otomatis create object 'user' baru

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from users.models import Profile 
 
 @csrf_exempt
 def login(request):
@@ -39,32 +40,20 @@ def register(request):
         password1 = data['password1']
         password2 = data['password2']
 
-        # Check if the passwords match
         if password1 != password2:
-            return JsonResponse({
-                "status": False,
-                "message": "Passwords do not match."
-            }, status=400)
-        
-        # Check if the username is already taken
+            return JsonResponse({"status": False, "message": "Passwords do not match."}, status=400)
+
         if User.objects.filter(username=username).exists():
-            return JsonResponse({
-                "status": False,
-                "message": "Username already exists."
-            }, status=400)
-        
-        # Create the new user
+            return JsonResponse({"status": False, "message": "Username already exists."}, status=400)
+
         user = User.objects.create_user(username=username, password=password1)
-        user.save()
-        
+
+        Profile.objects.create(user=user)
+
         return JsonResponse({
             "username": user.username,
-            "status": 'success',
+            "status": True,
             "message": "User created successfully!"
         }, status=200)
-    
-    else:
-        return JsonResponse({
-            "status": False,
-            "message": "Invalid request method."
-        }, status=400)
+
+    return JsonResponse({"status": False, "message": "Invalid request method."}, status=400)
